b958503f10600b8da2d6eb4df43dbffad65247a3
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-08-31 12:41:41 -0400

    completed assignment

--- src/hooks.js
+++ src/hooks.js
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function useFlip(initialFlipState = true) {
+const useFlip = (initialFlipState = true) => {
     const [isFlipped, setFlipped] = useState(initialFlipState);
   
     const flip = () => {
@@ -10,4 +11,32 @@ function useFlip(initialFlipState = true) {
     return [isFlipped, flip];
   }
 
-export { useFlip };
+const useAxios = (keyInLS, baseUrl) => {
+    const [responses, setResponses] = useLocalStorage(keyInLS);
+  
+    const addResponseData = async (formatter = data => data, restOfUrl = "") => {
+      const response = await axios.get(`${baseUrl}${restOfUrl}`);
+      setResponses(data => [...data, formatter(response.data)]);
+    };
+  
+    const clearResponses = () => setResponses([]);
+  
+    return [responses, addResponseData, clearResponses];
+  }
+
+const useLocalStorage = (key, initialValue = []) => {
+    if (localStorage.getItem(key)) {
+      initialValue = JSON.parse(localStorage.getItem(key));
+    }
+    const [value, setValue] = useState(initialValue);
+  
+    useEffect(() => {
+      localStorage.setItem(key, JSON.stringify(value));
+    }, [value, key]);
+  
+    return [value, setValue];
+  }
+  
+  export default useLocalStorage;
+  
+  export { useFlip, useAxios, useLocalStorage };
